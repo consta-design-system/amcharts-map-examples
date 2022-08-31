d364776594e8a9a214bef276a8fa8b9c8dc42eb5
N1MBER <socialfox33@gmail.com>
2022-08-31 15:03:33 +0300

    chore(amchartsmapadapter): add new stand and build tools
    consta-design-system/uikit#2597

---
+++ src/components/amChartsMapAdapter/__stand__/amChartsMapAdapter.stand.tsx
@@ -0,0 +1,9 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'amChartsMapAdapter',
+  id: 'amChartsMapAdapter',
+  group: 'adapters',
+  version: '0.0.0',
+  status: 'canary',
+});
