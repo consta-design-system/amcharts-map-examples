061a12bf5cb42125eccce3aafb88105aa9051a4f
gizeasy <gizeasy@gmail.com>
2022-09-27 16:36:17 +0300

    docs: edit docs (#2) (#3)
    Co-authored-by: arhayka <harlond@mail.ru>

--- src/components/Points/__stand__/Points.stand.tsx
+++ src/components/Points/__stand__/Points.stand.tsx
@@ -1,9 +1,8 @@
 import { createStand } from '##/stand/standConfig';
 
 export default createStand({
-  title: 'Points',
-  id: 'Points',
-  group: 'amcharts',
-  version: '0.0.0',
-  status: 'stable',
+  title: 'Точки',
+  id: 'points',
+  group: 'examples',
+  order: 20,
 });
--- src/docs/AmChartsMap/__stand__/AmChartsMap.stand.tsx
+++ src/docs/AmChartsMap/__stand__/AmChartsMap.stand.tsx
@@ -1,12 +1,11 @@
 import { createStand } from '##/stand/standConfig';
 
 export default createStand({
-  title: 'AmChartsMap',
+  title: 'Создание карт',
   id: 'AmChartsMap',
-  group: 'amcharts',
+  group: 'docs',
   sandbox: 'amchartsmapexample-x6cx2v',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FQIRjgPhpya2QbMtaheVnhe%2FConsta-Maps%3Fnode-id%3D0%253A1',
-  version: '0.0.0',
-  status: 'stable',
+  order: 20,
 });
